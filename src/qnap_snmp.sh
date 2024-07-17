3720276353de2d4e37a9d7718b99ed1e9d5ae7aa
Drew Miranda <drew.miranda@graylog.com>
2024-07-17 14:53:57 -0400

    Fix api calls failing because improper auth being used. must use SID returned from auth web req

--- src/qnap_snmp.sh
+++ src/qnap_snmp.sh
@@ -27,26 +27,30 @@ query_qnap(){
     ARG_COMMAND=$1
     case ${ARG_COMMAND} in
         auth)
-            curl --silent --cookie-jar cookie.txt -XPOST "${base_qnap_host}/cgi-bin/authLogin.cgi?user=${username}&plain_pwd=${pwd_in_plain_text}&remme=${remme}&service=${service}&device=${device}&force_to_check_2sv={force_to_check_2sv}" > /dev/null 2>&1
+            /usr/bin/curl --silent \
+                -XPOST \
+                --location "${base_qnap_host}/cgi-bin/authLogin.cgi?user=${username}&plain_pwd=${pwd_in_plain_text}&remme=${remme}&service=${service}&device=${device}&force_to_check_2sv=${force_to_check_2sv}" \
+                | grep -oP '\<authSid\>.*?\<\/authSid\>' | grep -oP 'CDATA\[(.*?)\]' | grep -oP '\[.*\]' | grep -oP '[^\[\]]+'
             ;;
         status)
-            query_qnap auth
-            curl --silent --cookie-jar cookie.txt -XPOST "${base_qnap_host}/cgi-bin/net/networkRequest.cgi?sid=s61i8lsk&subfunc=snmp" | grep -io \<snmp_enable\>.*\<\/snmp_enable\> | grep -io cdata\\[[[:digit:]]\\] | grep -io \\[[[:digit:]]\\] | grep -oP '0|1'
+            TOKEN=$(query_qnap auth)
+            /usr/bin/curl \
+                --silent \
+                -XPOST \
+                --location "${base_qnap_host}/cgi-bin/net/networkRequest.cgi?sid=${TOKEN}&subfunc=snmp" | grep -io \<snmp_enable\>.*\<\/snmp_enable\> | grep -io cdata\\[[[:digit:]]\\] | grep -io \\[[[:digit:]]\\] | grep -oP '0|1'
             ;;
         disable)
             query_qnap auth
-            curl --location "${base_qnap_host}/cgi-bin/net/networkRequest.cgi?sid=s61i8lsk&subfunc=snmp&apply=1" \
+            /usr/bin/curl --location "${base_qnap_host}/cgi-bin/net/networkRequest.cgi?sid=${TOKEN}&subfunc=snmp&apply=1" \
                 -v \
-                --cookie-jar \
                 --header 'Content-Type: text/plain' \
                 --data 'event_count=123&snmp_auth_protocol=0&select_snmp_version=v1&chkValue=0' \
                  > /dev/null 2>&1
             ;;
         enable)
             query_qnap auth
-            curl --location "${base_qnap_host}/cgi-bin/net/networkRequest.cgi?sid=s61i8lsk&subfunc=snmp&apply=1" \
+            /usr/bin/curl --location "${base_qnap_host}/cgi-bin/net/networkRequest.cgi?sid=${TOKEN}&subfunc=snmp&apply=1" \
                 -v \
-                --cookie-jar \
                 --header 'Content-Type: text/plain' \
                 --data 'chk_snmp=on&snmp_port=161&snmp_community=public&event_count=123&snmp_auth_protocol=0&select_snmp_version=v1&chkValue=0' \
                  > /dev/null 2>&1
