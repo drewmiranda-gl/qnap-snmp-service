eed3aea119f0f3386d20e7a28882563287993544
Drew Miranda <drew.miranda@graylog.com>
2024-08-15 17:52:58 -0400

    use new config filename

--- src/qnap_api.sh
+++ src/qnap_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-source ${SCRIPT_DIR}/qnap_snmp_config.sh
+source ${SCRIPT_DIR}/qnap_config.sh
 service=99
 device=remote-cli
 force_to_check_2sv=0
